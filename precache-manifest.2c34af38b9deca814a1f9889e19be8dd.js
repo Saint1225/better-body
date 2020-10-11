7bb6a965d106d9ef16c4b0c5d7d7966455bc5138
Saint1225 <vincent-valentine1225@hotmail.com>
2020-10-12 00:34:59 +0800

    Updates

---
+++ precache-manifest.2c34af38b9deca814a1f9889e19be8dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b8b1b10537030ffbd6f1146da74a1c5",
+    "url": "/better-body/index.html"
+  },
+  {
+    "revision": "f516dd09c6a70289762a",
+    "url": "/better-body/static/css/main.01500e0d.chunk.css"
+  },
+  {
+    "revision": "5b2190677cd9a60a1b31",
+    "url": "/better-body/static/js/2.41799bc2.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/better-body/static/js/2.41799bc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f516dd09c6a70289762a",
+    "url": "/better-body/static/js/main.c81dc1b1.chunk.js"
+  },
+  {
+    "revision": "c8a0960e0b5ffc2f2da7",
+    "url": "/better-body/static/js/runtime-main.b291666a.js"
+  }
+]);
